7aa79bded2182d489322fa9f446279dbb7684cee
Jose Garcia <47431411+Ruxwez@users.noreply.github.com>
2023-05-20 22:34:00 +0200

    title

--- src/components/window.bar.jsx
+++ src/components/window.bar.jsx
@@ -1,9 +1,22 @@
-import {} from "react";
+import { useEffect, useState } from "react";
 import s from "./styles/window.module.scss";
 import { appWindow } from "@tauri-apps/api/window";
+import { getName, getVersion } from "@tauri-apps/api/app";
 import { IconX } from "@tabler/icons-react";
 
 export default () => {
+  const [appName, setAppName] = useState("");
+  const [appVersion, setAppVersion] = useState("");
+
+  const getDataConfig = async () => {
+    setAppName(await getName());
+    setAppVersion(await getVersion());
+  };
+
+  useEffect(() => {
+    getDataConfig();
+  }, []);
+
   const minimize = () => {
     appWindow.minimize();
   };
@@ -14,7 +27,9 @@ export default () => {
 
   return (
     <div data-tauri-drag-region className={s.Bar}>
-      <h1 data-tauri-drag-region>QSValidator</h1>
+      <h1 data-tauri-drag-region>
+        {appName} {appVersion}
+      </h1>
       <div className={s.Right}>
         <button onClick={minimize} className={s.Minimize}>
           <span></span>
